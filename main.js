edcd47b0924b73207ac87c617d1b7858006b8f4e
Jason5564 <96456170+Jason5564@users.noreply.github.com>
2023-03-03 18:13:26 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function gofb() {
+    window.open("https://www.facebook.com/Genshinimpact.tw","_blank").focus();
+}
+function gotwitter(){
+    window.open("https://twitter.com/GenshinImpact","_blank").focus();
+}
+
+function goIG(){
+    window.open("https://www.instagram.com/genshinimpact/","_blank").focus();
+}
+
+function goYT(){
+    window.open("https://www.youtube.com/@GenshinImpact","_blank").focus();
+}
+
+function goTT(){
+    window.open("https://www.tiktok.com/@genshinimpact_tw","_blank").focus();
+}
+
